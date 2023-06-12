75b612e56700e73cb2cce525dedbf9f934772a5e
Peyton Boggs <peytonboggs@RTSLabss-MacBook-Pro.local>
2023-06-12 16:35:16 -0400

    Organization and componentization of files

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import React, {useEffect, useState} from 'react';
-
-interface Pokemon {
-  name: string;
-}
+import AppHeader from './Title';
+import Menu from './Menu';
 
 function App() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-  
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-      .then(response => response.json())
-      .then(data => {
-        const firstTenPokemon = data.results.slice(0, 10);
-        setPokemon(firstTenPokemon);
-      });
-  };
-
   return (
       <div>
-        <h1>PokeBake</h1>
-        <div>
-        {pokemon.map(poke => (
-          <>
-          <button>{poke.name}</button>
-          <br></br>
-          <br></br>
-          </>
-        ))}
-        </div>
+        <AppHeader />
+        <Menu />
       </div>
   )
 }
