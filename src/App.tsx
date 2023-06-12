285453b02b55077bf66b02eb9e32708a370e4e5c
Peyton Boggs <peytonboggs@RTSLabss-MacBook-Pro.local>
2023-06-12 13:42:50 -0400

    Added a menu with 10 labeled buttons to App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ function App() {
   return (
       <div>
         <h1>PokeBake</h1>
+        <button>Caterpie</button><br></br>
+        <button>Pidgey</button><br></br>
+        <button>Pikachu</button><br></br>
+        <button>Jigglypuff</button><br></br>
+        <button>Oddish</button><br></br>
+        <button>Diglett</button><br></br>
+        <button>Psyduck</button><br></br>
+        <button>Bellsprout</button><br></br>
+        <button>Geodude</button><br></br>
+        <button>Snorlax</button><br></br>
       </div>
   );
 }
