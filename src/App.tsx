d9f7ec4c90b71c71179d31daa0919ba9dcb8e6e1
Peyton Boggs <peytonboggs@RTSLabss-MacBook-Pro.local>
2023-06-12 14:57:51 -0400

    Generated list of 10 buttons using pokeapi.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
+import React, {useEffect, useState} from 'react';
+
+interface Pokemon {
+  name: string;
+}
+
 function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then(response => response.json())
+      .then(data => {
+        const firstTenPokemon = data.results.slice(0, 10);
+        setPokemon(firstTenPokemon);
+      });
+  };
+
   return (
       <div>
         <h1>PokeBake</h1>
-        <button>Caterpie</button><br></br>
-        <button>Pidgey</button><br></br>
-        <button>Pikachu</button><br></br>
-        <button>Jigglypuff</button><br></br>
-        <button>Oddish</button><br></br>
-        <button>Diglett</button><br></br>
-        <button>Psyduck</button><br></br>
-        <button>Bellsprout</button><br></br>
-        <button>Geodude</button><br></br>
-        <button>Snorlax</button><br></br>
+        <div>
+        {pokemon.map(poke => (
+          <>
+          <button>{poke.name}</button>
+          <br></br>
+          <br></br>
+          </>
+        ))}
+        </div>
       </div>
-  );
+  )
 }
 
 export default App;
