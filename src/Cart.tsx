02cd6eab161a9bdfe8d583b29f702b990f477a89
Peyton Boggs <peytonboggs@RTSLabss-MacBook-Pro.local>
2023-06-20 08:53:10 -0400

    Changed the team list to have the card design of the menu

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -7,17 +7,22 @@ interface CartProps {
   
   
 export default function Cart({cart, handleRemove}: CartProps) {
-    return (
-      <div className='cart'>
-       <h2 className="heading">Team:</h2>
-       {cart.map(poke =>
-          <div key={poke.id}>
-            <div className='siblings'>
-             <button className="removeButton" onClick={() => handleRemove(poke)}>X</button>
-             <p className='selectedName'>{poke.name}</p>
-            </div>
+  return (
+    <div>
+      <h2 className="heading">Team:</h2>
+      <div className='menu'>
+        {cart.map(poke =>
+        <div className="pokeInterface" key={poke.id}>
+          <div className='lineOne'>
+            <p className='number'>#{poke.id}</p>
+            <img className="ball" src={poke.sprite} alt={poke.name}></img>
           </div>
+          <p className='types'>type: {poke.types.join(", ")}</p>
+          <button className="removeButton" onClick={() => handleRemove(poke)}>{poke.name}</button>
+          <br></br>
+         </div>
         )}
       </div>
-     )
+    </div>
+  )
 }
